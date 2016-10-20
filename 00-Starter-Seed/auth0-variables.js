60c2148251d4c2d4b0d7412587f1356e1917c205
Joe Aliperti <joe@clearstoneip.com>
2016-10-20 15:01:19 -0700

    client id and fixed dependency

--- 00-Starter-Seed/auth0-variables.js
+++ 00-Starter-Seed/auth0-variables.js
@@ -1,3 +1,3 @@
-var AUTH0_CLIENT_ID='undefined'; 
-var AUTH0_DOMAIN='undefined'; 
+var AUTH0_CLIENT_ID='iSaF7eYZp2elD4NCwokoaSM1DuUu8R7e'; 
+var AUTH0_DOMAIN='rapidinsight.auth0.com'; 
 var AUTH0_CALLBACK_URL=location.href;
